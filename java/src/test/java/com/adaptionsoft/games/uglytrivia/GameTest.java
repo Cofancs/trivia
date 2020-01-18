7ca140b8da3ab3218e4bea0a693a82201fec5e96
Zsofia Edina Nemeth <zsofia_edina_nemeth@epam.com>
2020-01-18 10:48:15 +0100

    added tests for howmanyPlayers and add methods

--- java/src/test/java/com/adaptionsoft/games/uglytrivia/GameTest.java
+++ java/src/test/java/com/adaptionsoft/games/uglytrivia/GameTest.java
@@ -1,12 +1,47 @@
 package com.adaptionsoft.games.uglytrivia;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
 public class GameTest {
 
+  private Game underTest;
+
   @Before
   public void setUp() throws Exception {
+    underTest = new Game();
+  }
+
+  @Test
+  public void testAdd() {
+    //GIVEN
+
+    //WHEN
+    boolean actual = underTest.add("test");
+    //THEN
+    assertFalse(underTest.players.isEmpty());
+    int[] places = underTest.places;
+    int[] expectedPlaces = new int[6];
+    expectedPlaces[1] = 0;
+    assertArrayEquals(expectedPlaces, places);
+    assertTrue(true);
+
+  }
+
+  @Test
+  public void testHowManyPlayer() {
+    //GIVEN
+    ArrayList players = underTest.players;
+    players.add("test");
+    players.add("test2");
+
+    //WHEN
+    int actual = underTest.howManyPlayers();
+    //THEN
+    assertEquals(2, actual);
   }
 }
