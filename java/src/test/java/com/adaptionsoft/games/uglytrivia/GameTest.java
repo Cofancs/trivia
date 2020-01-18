44b2945935ee93713b7fcf95898c18bd9a356dea
Szabó Gábor <szaboxgabor@gmail.com>
2020-01-18 10:59:28 +0100

    added penalty box unit tests

--- java/src/test/java/com/adaptionsoft/games/uglytrivia/GameTest.java
+++ java/src/test/java/com/adaptionsoft/games/uglytrivia/GameTest.java
@@ -6,44 +6,56 @@ import org.junit.Test;
 
 public class GameTest {
 
-  private Game underTest;
+    private Game underTest;
 
-  @Before
-  public void setUp() throws Exception {
-    underTest = new Game();
-  }
+    @Before
+    public void setUp() throws Exception {
+        underTest = new Game();
+    }
 
-  @Test
-  public void wasCorrectlyAnsweredAddsCoinToCurrentPlayer(){
-    //GIVEN
-    underTest.add("testPlayer");
-    //WHEN
-    underTest.wasCorrectlyAnswered();
-    //THEN
-    Assert.assertEquals(1, underTest.purses[0]);
-  }
+    @Test
+    public void wasCorrectlyAnsweredAddsCoinToCurrentPlayer() {
+        //GIVEN
+        underTest.add("testPlayer");
+        //WHEN
+        underTest.wasCorrectlyAnswered();
+        //THEN
+        Assert.assertEquals(1, underTest.purses[0]);
+    }
 
-  @Test
-  public void nextPlayerWasCalledAfterCorrectAnswer(){
-    //GIVEN
-    underTest.add("testPlayer1");
-    underTest.add("testPlayer2");
-    //WHEN
-    underTest.wasCorrectlyAnswered();
-    //THEN
-    Assert.assertEquals(1, underTest.currentPlayer);
-  }
+    @Test
+    public void nextPlayerWasCalledAfterCorrectAnswer() {
+        //GIVEN
+        underTest.add("testPlayer1");
+        underTest.add("testPlayer2");
+        //WHEN
+        underTest.wasCorrectlyAnswered();
+        //THEN
+        Assert.assertEquals(1, underTest.currentPlayer);
+    }
 
-  @Test
-  public void nextPlayerIsFirstPlayerWhenLastPlayerAnsweredCorrectly(){
-    //GIVEN
-    underTest.add("testPlayer1");
-    underTest.add("testPlayer2");
-    underTest.add("testPlayer3");
-    underTest.currentPlayer = 2;
-    //WHEN
-    underTest.wasCorrectlyAnswered();
-    //THEN
-    Assert.assertEquals(0, underTest.currentPlayer);
-  }
+    @Test
+    public void nextPlayerIsFirstPlayerWhenLastPlayerAnsweredCorrectly() {
+        //GIVEN
+        underTest.add("testPlayer1");
+        underTest.add("testPlayer2");
+        underTest.add("testPlayer3");
+        underTest.currentPlayer = 2;
+        //WHEN
+        underTest.wasCorrectlyAnswered();
+        //THEN
+        Assert.assertEquals(0, underTest.currentPlayer);
+    }
+
+    @Test
+    void gettingOutFromPenaltyBoxWithCourrentAnswerAddsCoin() {
+        //GIVEN
+        underTest.add("testPlayer1");
+        underTest.inPenaltyBox[0] = true;
+        underTest.isGettingOutOfPenaltyBox = true;
+        //WHEN
+        underTest.wasCorrectlyAnswered();
+        //THEN
+        Assert.assertEquals(1, underTest.purses[0]);
+    }
 }
