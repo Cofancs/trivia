85fd9f067f5747f8c805c041ad1c4df0e14b0198
Zsofia Edina Nemeth <zsofia_edina_nemeth@epam.com>
2020-01-18 12:03:05 +0100

    and refactored GameRunner code

--- java/src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
+++ java/src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
@@ -11,17 +11,15 @@ public class GameRunner {
   private static boolean notAWinner;
 
   public static void main(String[] args) {
-    play();
+    play(new Random());
   }
 
-  public static void play() {
+  public static void play(Random rand) {
     Game aGame = new Game();
 
     aGame.add("Chet");
     aGame.add("Pat");
     aGame.add("Sue");
-
-    Random rand = new Random();
 
     do {
       aGame.roll(rand.nextInt(5) + 1);
