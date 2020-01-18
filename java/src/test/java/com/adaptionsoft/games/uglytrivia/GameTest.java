dbe6cff5e61bb066121ea3c80641cb9397fb0abb
Zsofia Edina Nemeth <zsofia_edina_nemeth@epam.com>
2020-01-18 11:41:48 +0100

    fixed code: removed extra}

--- java/src/test/java/com/adaptionsoft/games/uglytrivia/GameTest.java
+++ java/src/test/java/com/adaptionsoft/games/uglytrivia/GameTest.java
@@ -7,37 +7,35 @@ import org.junit.Test;
 import java.util.ArrayList;
 
 import static org.junit.Assert.*;
-import static org.junit.Assert.assertEquals;
 
 public class GameTest {
 
-    private Game underTest;
+  private Game underTest;
 
-    @Before
-    public void setUp() throws Exception {
-        underTest = new Game();
-    }
+  @Before
+  public void setUp() throws Exception {
+    underTest = new Game();
+  }
 
-    @Test
-    public void wasCorrectlyAnsweredAddsCoinToCurrentPlayer() {
-        //GIVEN
-        underTest.add("testPlayer");
-        //WHEN
-        underTest.wasCorrectlyAnswered();
-        //THEN
-        Assert.assertEquals(1, underTest.purses[0]);
-    }
+  @Test
+  public void wasCorrectlyAnsweredAddsCoinToCurrentPlayer() {
+    //GIVEN
+    underTest.add("testPlayer");
+    //WHEN
+    underTest.wasCorrectlyAnswered();
+    //THEN
+    Assert.assertEquals(1, underTest.purses[0]);
+  }
 
-    @Test
-    public void nextPlayerWasCalledAfterCorrectAnswer() {
-        //GIVEN
-        underTest.add("testPlayer1");
-        underTest.add("testPlayer2");
-        //WHEN
-        underTest.wasCorrectlyAnswered();
-        //THEN
-        Assert.assertEquals(1, underTest.currentPlayer);
-    }
+  @Test
+  public void nextPlayerWasCalledAfterCorrectAnswer() {
+    //GIVEN
+    underTest.add("testPlayer1");
+    underTest.add("testPlayer2");
+    //WHEN
+    underTest.wasCorrectlyAnswered();
+    //THEN
+    Assert.assertEquals(1, underTest.currentPlayer);
   }
 
   @Test
@@ -68,84 +66,85 @@ public class GameTest {
     //THEN
     assertEquals(2, actual);
   }
-    @Test
-    public void nextPlayerIsFirstPlayerWhenLastPlayerAnsweredCorrectly() {
-        //GIVEN
-        underTest.add("testPlayer1");
-        underTest.add("testPlayer2");
-        underTest.add("testPlayer3");
-        underTest.currentPlayer = 2;
-        //WHEN
-        underTest.wasCorrectlyAnswered();
-        //THEN
-        Assert.assertEquals(0, underTest.currentPlayer);
-    }
 
-    @Test
-    public void gettingOutFromPenaltyBoxWithCorrectAnswerAddsCoin() {
-        //GIVEN
-        underTest.add("testPlayer1");
-        underTest.inPenaltyBox[0] = true;
-        underTest.isGettingOutOfPenaltyBox = true;
-        //WHEN
-        underTest.wasCorrectlyAnswered();
-        //THEN
-        Assert.assertEquals(1, underTest.purses[0]);
-    }
+  @Test
+  public void nextPlayerIsFirstPlayerWhenLastPlayerAnsweredCorrectly() {
+    //GIVEN
+    underTest.add("testPlayer1");
+    underTest.add("testPlayer2");
+    underTest.add("testPlayer3");
+    underTest.currentPlayer = 2;
+    //WHEN
+    underTest.wasCorrectlyAnswered();
+    //THEN
+    Assert.assertEquals(0, underTest.currentPlayer);
+  }
 
-    @Test
-    public void gettingOutFromPenaltyBoxWithCorrectAnswerCallsNextPlayer() {
-        //GIVEN
-        underTest.add("testPlayer1");
-        underTest.add("testPlayer2");
-        underTest.inPenaltyBox[0] = true;
-        underTest.isGettingOutOfPenaltyBox = true;
-        //WHEN
-        underTest.wasCorrectlyAnswered();
-        //THEN
-        Assert.assertEquals(1, underTest.currentPlayer);
-    }
+  @Test
+  public void gettingOutFromPenaltyBoxWithCorrectAnswerAddsCoin() {
+    //GIVEN
+    underTest.add("testPlayer1");
+    underTest.inPenaltyBox[0] = true;
+    underTest.isGettingOutOfPenaltyBox = true;
+    //WHEN
+    underTest.wasCorrectlyAnswered();
+    //THEN
+    Assert.assertEquals(1, underTest.purses[0]);
+  }
 
-    @Test
-    public void gettingOutFromPenaltyBoxWithLastPlayerCallsFirstPlayer() {
-        //GIVEN
-        underTest.add("testPlayer1");
-        underTest.add("testPlayer2");
-        underTest.add("testPlayer3");
-        underTest.currentPlayer = 2;
-        underTest.inPenaltyBox[2] = true;
-        underTest.isGettingOutOfPenaltyBox = true;
-        //WHEN
-        underTest.wasCorrectlyAnswered();
-        //THEN
-        Assert.assertEquals(0, underTest.currentPlayer);
-    }
+  @Test
+  public void gettingOutFromPenaltyBoxWithCorrectAnswerCallsNextPlayer() {
+    //GIVEN
+    underTest.add("testPlayer1");
+    underTest.add("testPlayer2");
+    underTest.inPenaltyBox[0] = true;
+    underTest.isGettingOutOfPenaltyBox = true;
+    //WHEN
+    underTest.wasCorrectlyAnswered();
+    //THEN
+    Assert.assertEquals(1, underTest.currentPlayer);
+  }
 
-    @Test
-    public void penaltyBoxWithoutGettingOutFlagCallsNextPlayer() {
-        //GIVEN
-        underTest.add("testPlayer1");
-        underTest.add("testPlayer2");
-        underTest.inPenaltyBox[0] = true;
-        underTest.isGettingOutOfPenaltyBox = false;
-        //WHEN
-        underTest.wasCorrectlyAnswered();
-        //THEN
-        Assert.assertEquals(1, underTest.currentPlayer);
-    }
+  @Test
+  public void gettingOutFromPenaltyBoxWithLastPlayerCallsFirstPlayer() {
+    //GIVEN
+    underTest.add("testPlayer1");
+    underTest.add("testPlayer2");
+    underTest.add("testPlayer3");
+    underTest.currentPlayer = 2;
+    underTest.inPenaltyBox[2] = true;
+    underTest.isGettingOutOfPenaltyBox = true;
+    //WHEN
+    underTest.wasCorrectlyAnswered();
+    //THEN
+    Assert.assertEquals(0, underTest.currentPlayer);
+  }
 
-    @Test
-    public void penaltyBoxWithoutGettingOutFlagWithLastPlayerCallsFirstPlayer() {
-        //GIVEN
-        underTest.add("testPlayer1");
-        underTest.add("testPlayer2");
-        underTest.add("testPlayer3");
-        underTest.currentPlayer = 2;
-        underTest.inPenaltyBox[2] = true;
-        underTest.isGettingOutOfPenaltyBox = false;
-        //WHEN
-        underTest.wasCorrectlyAnswered();
-        //THEN
-        Assert.assertEquals(0, underTest.currentPlayer);
-    }
+  @Test
+  public void penaltyBoxWithoutGettingOutFlagCallsNextPlayer() {
+    //GIVEN
+    underTest.add("testPlayer1");
+    underTest.add("testPlayer2");
+    underTest.inPenaltyBox[0] = true;
+    underTest.isGettingOutOfPenaltyBox = false;
+    //WHEN
+    underTest.wasCorrectlyAnswered();
+    //THEN
+    Assert.assertEquals(1, underTest.currentPlayer);
+  }
+
+  @Test
+  public void penaltyBoxWithoutGettingOutFlagWithLastPlayerCallsFirstPlayer() {
+    //GIVEN
+    underTest.add("testPlayer1");
+    underTest.add("testPlayer2");
+    underTest.add("testPlayer3");
+    underTest.currentPlayer = 2;
+    underTest.inPenaltyBox[2] = true;
+    underTest.isGettingOutOfPenaltyBox = false;
+    //WHEN
+    underTest.wasCorrectlyAnswered();
+    //THEN
+    Assert.assertEquals(0, underTest.currentPlayer);
+  }
 }
